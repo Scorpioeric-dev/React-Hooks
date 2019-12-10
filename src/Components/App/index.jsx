b107ddc0a79c4b25d6d0ed238647bc36d808da98
Scorpioeric-dev <ericscorpio1@gmail.com>
2019-12-09 18:56:32 -0700

    hooks

--- src/Components/App/index.jsx
+++ src/Components/App/index.jsx
@@ -1,6 +1,39 @@
-import React, { Component } from 'react';
-export default function App({}){
-return <div>
-Hello React App
- </div>
+import React, { useState, useEffect } from "react";
+export default function App({}) {
+  const formDefaultValues = {
+    email: "",
+    fullName: ""
+  };
+  const [formValues, setFormValues] = useState(formDefaultValues);
+  const { email, fullName } = formValues;
+  useEffect(() => {
+    console.dir({ email, fullName });
+  }, [formValues]);
+
+   function handleChange(e) {
+    const target = e.target;
+    setFormValues(prevState => ({
+      ...prevState,
+      [target.name]: target.value
+    }));
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={email}
+        name={"email"}
+        onChange={handleChange}
+        placeholder="Email"
+      />
+      <input
+        type="text"
+        value={fullName}
+        name={"fullName"}
+        onChange={handleChange}
+        placeholder="FullName"
+      />
+    </div>
+  );
 }
